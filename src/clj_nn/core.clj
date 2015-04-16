9c0a9cc0c997e5ba2d781461f5d817d52947ca9c
Pradnyesh Sawant <spradnyesh@gmail.com>
2015-04-16 14:59:19 +0530

    init

---
+++ src/clj_nn/core.clj
@@ -0,0 +1,6 @@
+(ns clj-nn.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_nn/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-nn.core-test
+  (:require [clojure.test :refer :all]
+            [clj-nn.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
